9f2e1dafdef9ac9de311c5650d7078bf99f20610
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-04-09 15:02:59 -0400

    Update video.rs
    Created DownloadProgress struct to parse the necessary information from yt-dlp's stdout output and transport it back to a rust-vdl component.
    Updated spawnCommand to include the --newline option so the download progress is printed on new lines instead of updating a single line; Makes it simpler to parse in real time.
    Renamed writeOutput to processOutput

--- rust-vdl/src/download/video.rs
+++ rust-vdl/src/download/video.rs
@@ -37,49 +37,61 @@ impl VideoDownloader
 			"-P", self.outputDirectory.as_str(),
 			"-o", self.outputTemplate.get().as_str(),
 			video.as_str(),
-		]);
+		].as_mut());
 		
 		match proc
 		{
-			Ok(mut child) => self.writeOutput(child.stdout.take(), child.stderr.take()),
+			Ok(mut child) => self.processOutput(child.stdout.take(), child.stderr.take()),
 			Err(e) => println!("Error downloading video: {} -> {}", video, e)
 		};
 	}
 	
 	pub fn listFormats(&self, video: String)
 	{
-		let proc = self.spawnCommand(vec!["-F", video.as_str()]);
+		let proc = self.spawnCommand(vec!["-F", video.as_str()].as_mut());
 		
 		match proc
 		{
-			Ok(mut child) => self.writeOutput(child.stdout.take(), child.stderr.take()),
+			Ok(mut child) => self.processOutput(child.stdout.take(), child.stderr.take()),
 			Err(e) => println!("Error getting list of available formats for video: {} -> {}", video, e)
 		};
 	}
 	
-	fn spawnCommand(&self, args: Vec<&str>) -> io::Result<Child>
+	fn spawnCommand(&self, args: &mut Vec<&str>) -> io::Result<Child>
 	{
+		let mut finalArgs = vec!["--newline"];
+		finalArgs.append(args);
+		
 		return Command::new(self.binary.to_owned())
 			.stderr(Stdio::piped())
 			.stdout(Stdio::piped())
-			.args(args)
+			.args(finalArgs)
 			.spawn();
 	}
 	
-	fn writeOutput(&self, stdout: Option<ChildStdout>, stderr: Option<ChildStderr>)
+	fn processOutput(&self, stdout: Option<ChildStdout>, stderr: Option<ChildStderr>)
 	{
 		match stdout
 		{
 			Some(so) => {
-				let lines = BufReader::new(so).lines();
-				for line in lines
-				{
-					match line
-					{
-						Ok(s) =>  println!("{}", s),
-						Err(e) => println!("{}", e),
-					}
-				}
+				
+				let reader = BufReader::new(so);
+				reader.lines()
+					.filter_map(|l| l.ok())
+					.for_each(|l| {
+						if l.starts_with("[download]")
+						{
+							let progress = DownloadProgress::from(l.to_owned());
+							if progress.isValid()
+							{
+								println!("{}", progress);
+							}
+						}
+						else
+						{
+							println!("{}", l);
+						}
+					});
 			},
 			None => println!("No ChildStdout"),
 		};
@@ -87,17 +99,71 @@ impl VideoDownloader
 		match stderr
 		{
 			Some(se) => {
-				let lines = BufReader::new(se).lines();
-				for line in lines
-				{
-					match line
-					{
-						Ok(s) =>  println!("{}", s),
+				let reader = BufReader::new(se);
+				reader.lines()
+					.for_each(|l| match l {
+						Ok(o) => println!("{}", o),
 						Err(e) => println!("{}", e),
-					}
-				}
+					});
 			},
 			None => println!("No ChildStderr"),
 		}
 	}
+}
+
+// --------------------------------------------------
+
+#[derive(Clone, Debug, Default)]
+pub struct DownloadProgress
+{
+	pub estimatedSize: String,
+	pub estimatedTime: String,
+	pub fragmentStatus: String,
+	pub percentComplete: String,
+	pub transferRate: String,
+}
+
+impl DownloadProgress
+{
+	fn isValid(&self) -> bool
+	{
+		return !self.percentComplete.is_empty();
+	}
+	
+	fn toString(&self) -> String
+	{
+		return format!("{} {} {} {}", self.transferRate, self.estimatedSize, self.estimatedTime, self.fragmentStatus);
+	}
+}
+
+impl std::fmt::Display for DownloadProgress
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+	{
+		return f.write_str(self.toString().as_str());
+    }
+}
+
+impl From<String> for DownloadProgress
+{
+	fn from(value: String) -> Self
+	{
+		let mut instance = Self::default();
+		
+		value.split_whitespace()
+			.for_each(|w| {
+				w.ends_with("%").then(|| instance.percentComplete = w.to_owned());
+				w.ends_with("B").then(|| instance.estimatedSize = w.to_owned());
+				w.ends_with("B/s").then(|| instance.transferRate = w.to_owned());
+				w.find(":").is_some().then(|| instance.estimatedTime = w.to_owned());
+				w.ends_with(")").then(|| instance.fragmentStatus = w[..w.len()-1].to_owned());
+			});
+		
+		return instance;
+	}
+}
+
+impl Into<String> for DownloadProgress
+{
+	fn into(self) -> String { return self.toString(); }
 }
